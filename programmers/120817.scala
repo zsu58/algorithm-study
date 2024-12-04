9bc71c8b5835c8cfcb856ba894a039fe21dcb82e
zsu58 <zsu58@icloud.com>
2024-12-04 23:00:18 +0900

    2024-12_04_1

---
+++ programmers/120817.scala
@@ -0,0 +1,6 @@
+/* https://school.programmers.co.kr/learn/courses/30/lessons/120817 */
+object Solution {
+    def solution(numbers: Vector[Int]): Double = {
+        (numbers.sum * 1.0) / numbers.size
+    }
+}
